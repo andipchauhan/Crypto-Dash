be3dc776baaa1ce2bbeda995b7f11e4e1c42f8d8
Andip Chauhan <chauhan2244andip@gmail.com>
2025-09-30 11:11:50 +0530

    Displayed fetched data, added CoinCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-const API_URL = import.meta.env.VITE_API_URL
+import CoinCard from "./components/CoinCard";
 
+const API_URL = import.meta.env.VITE_API_URL
 const App = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -27,26 +28,20 @@ const App = () => {
     fetchCoins();
   },[])
 
-  // useEffect(() => {
-  //   fetch(`${API_URL}&order=market_cap_desc&per_page=10&page=1&sparkline=false`)
-  //   .then((response) => {
-  //     if(!response.ok) throw new Error('Failed to fetch data')
-  //       // response.ok is true only if the status code is 200–299 (the “successful” range).
-  //     return response.json()
-  //   })
-  //   .then((data) => {
-  //     console.log(data)
-  //     setCoins(data)
-  //     setLoading(false)
-  //   })
-  //   .catch((err) => {
-  //     setError(err.message)
-  //     setLoading(false)
-  //   })
-  // },[])
-
   return ( 
-    <div><h1>🚀 Crypto Dash</h1></div>
+    <div><h1>🚀 Crypto Dash</h1>
+    {loading && <p>Loading...</p>}
+    {error && <div className="error">{error}</div>}
+    {!loading && !error && (
+      <main className="grid">
+        {
+          coins.map((coin)=> (
+            <CoinCard coin={coin} key={coin.id}/>
+          ))
+        }
+      </main>
+    )}
+    </div>
    );
 }
  
