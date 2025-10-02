de3d41499a9c40baf7e78aae2640bb888171044a
andipchauhan <chauhan2244andip@gmail.com>
2025-10-02 23:56:39 +0530

    Added Routes and Link, Header component

---
+++ src/pages/aboutPage.jsx
@@ -0,0 +1,11 @@
+const AboutPage = () => {
+    return ( 
+        <div className="about">
+            <h1>About Crypto Dash</h1>
+            <p>This is a React project that shows live crypto prices.</p>
+            <p>🚀 Expect more features such as change currency in future.</p>
+        </div>
+     );
+}   
+
+export default AboutPage;
